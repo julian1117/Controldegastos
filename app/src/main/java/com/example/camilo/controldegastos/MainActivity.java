e14363cfcb79a632c6a2a68eeca1f36ba87e9aac
julian1117 <julian5296@gmail.com>
2017-08-03 16:13:43 -0500

    ventana 2 / comobox

--- app/src/main/java/com/example/camilo/controldegastos/MainActivity.java
+++ app/src/main/java/com/example/camilo/controldegastos/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.camilo.controldegastos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        llenarComboMarca();
+        llenarComboTipoVehiculo();
     }
+
+
+    public void llenarComboMarca() {
+        Spinner cbmarca = (Spinner) findViewById(R.id.marca);
+        String[] lista = {"Chevrolet", "Mazda", "Audi", "Mercedes Benz"};
+        ArrayAdapter<String> spinnerMarca = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, lista);
+        cbmarca.setAdapter(spinnerMarca);
+    }
+
+    public void llenarComboTipoVehiculo() {
+        Spinner cbmarca = (Spinner) findViewById(R.id.tipoVehiculo);
+        String[] lista = {"Compacto (uber)", "Bus", "Micro Bus", "Taxi"};
+        ArrayAdapter<String> spinnerMarca = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, lista);
+        cbmarca.setAdapter(spinnerMarca);
+    }
+
+
 }
