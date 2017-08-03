e9260566781bda339cc0e947c27d797aaebaa302
LauraPosada <posadalaura998@gmail.com>
2017-08-03 16:24:35 -0500

    VENTANA 5

---
+++ app/src/main/java/com/example/camilo/controldegastos/InformacionVehiculo.java
@@ -0,0 +1,13 @@
+package com.example.camilo.controldegastos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InformacionVehiculo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_informacion_vehiculo);
+    }
+}
