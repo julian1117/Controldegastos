d28b6d34e7bbfcbc8ff5b31e13bf5ae5dd90142c
julian1117 <julian5296@gmail.com>
2017-08-05 14:54:51 -0500

    ventana 5 /  con botones

--- app/src/main/java/com/example/camilo/controldegastos/MainActivity.java
+++ app/src/main/java/com/example/camilo/controldegastos/MainActivity.java
@@ -1,23 +1,35 @@
 package com.example.camilo.controldegastos;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences persistencia;
+    Spinner cbmarca;
+
+    EditText placa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        placa = (EditText) findViewById(R.id.etPlaca);
         llenarComboMarca();
         llenarComboTipoVehiculo();
     }
 
 
     public void llenarComboMarca() {
-        Spinner cbmarca = (Spinner) findViewById(R.id.marca);
+        cbmarca = (Spinner) findViewById(R.id.marca);
         String[] lista = {"Chevrolet", "Mazda", "Audi", "Mercedes Benz"};
         ArrayAdapter<String> spinnerMarca = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item, lista);
@@ -32,5 +44,20 @@ public class MainActivity extends AppCompatActivity {
         cbmarca.setAdapter(spinnerMarca);
     }
 
+    public  void guardarDato(){
+        /**
+        persistencia = getSharedPreferences("registroVehiculo", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = persistencia.edit();
+        editor.putString("placa",placa.getText().toString());
+        editor.putString("Marca", cbmarca.getSelectedItem().toString());
+*/
+        String b = cbmarca.getSelectedItem().toString();
+        Toast.makeText(this, "Ingreso" +b, Toast.LENGTH_SHORT).show();
+
+      //  editor.putInt("edad",Integer.parseInt(edad.getText().toString()));
+       // editor.commit();
+
+
+    }
 
 }
