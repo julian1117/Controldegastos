f2e1dad129e8ba1c96608cbe77d7eb0fa629542d
LauraPosada <posadalaura998@gmail.com>
2017-08-06 13:05:12 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/example/camilo/controldegastos/ListadoVehiculos.java

--- app/src/main/java/com/example/camilo/controldegastos/InformacionVehiculo.java
+++ app/src/main/java/com/example/camilo/controldegastos/InformacionVehiculo.java
@@ -1,13 +1,40 @@
 package com.example.camilo.controldegastos;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class InformacionVehiculo extends AppCompatActivity {
+
+    EditText plac;
+    EditText marc;
+    EditText tipo;
+
+    private SharedPreferences persistencia;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_informacion_vehiculo);
+
+
+
+        plac = (EditText) findViewById(R.id.etPlaca);
+        marc = (EditText) findViewById(R.id.edMarca);
+        tipo = (EditText) findViewById(R.id.edTipoVeGas);
+        cargar();
+
+    }
+
+    public void cargar (){
+
+        persistencia=getSharedPreferences("registroVehiculo", Context.MODE_PRIVATE);
+        plac.setText(persistencia.getString("placa",""));
+        marc.setText(persistencia.getString("Tipo vehiculo", " "));
+        //marc.setText(persistencia.getString(""));
     }
 }
--- app/src/main/java/com/example/camilo/controldegastos/ListadoVehiculos.java
+++ app/src/main/java/com/example/camilo/controldegastos/ListadoVehiculos.java
@@ -1,9 +1,15 @@
 package com.example.camilo.controldegastos;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -22,19 +28,36 @@ public class ListadoVehiculos extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listado_vehiculos);
 
-        persistencia=getSharedPreferences("registroVehiculo", Context.MODE_PRIVATE);
+        persistencia = getSharedPreferences("registroVehiculo", Context.MODE_PRIVATE);
         listaVehiculos = (ListView) findViewById(R.id.listadoVehiculos);
 
-        datos =new ArrayList<String>();
+        datos = new ArrayList<String>();
         int ban = 0;
-        do{
-            datos.add(persistencia.getString("placa", " ") +" - "+persistencia.getString("Tipo vehiculo", " ") );
+        do {
+            datos.add(persistencia.getString("placa", " ") + " - " + persistencia.getString("Tipo vehiculo", " "));
 
-            ban ++;
-        }while(persistencia.getAll().size()>ban);
+            ban++;
+        } while (persistencia.getAll().size() > ban);
 
 
-        adaptador1=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, datos);
+        adaptador1 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, datos);
         listaVehiculos.setAdapter(adaptador1);
+
+      listaVehiculos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+          @Override
+          public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+             String a = adaptador1.getItem(position);
+
+              ir(view);
+          }
+      });
+
+    }
+
+
+    public void ir(View v){
+        Intent intent = new Intent(this,InformacionVehiculo.class);
+        startActivity(intent);
     }
 }
